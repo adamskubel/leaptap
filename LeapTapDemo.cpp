4a33ae0f27e38ddec34a7a3af244f350a9c084a7
Adam <adamskubel@gmail.com>
2013-08-19 23:52:08 -0400

    Leap tap demo

---
+++ LeapTapDemo.cpp
@@ -0,0 +1,79 @@
+#include <Leap.h>
+
+using namespace Leap;
+
+Frame lastFrame;
+void onPolledFrame(const Controller & controller)
+{
+	int MaxHistory = 100;
+	for (int i=1;i < MaxHistory; i++)
+		{
+			Frame frame = controller.frame(i-1);
+			Frame previousFrame = controller.frame(i);
+
+			if (!previousFrame.isValid())
+				break;
+
+	for (int p = 0; p < frame.pointables().count();p++)
+	{
+		Pointable pN = frame.pointables()[p];
+	Pointable pN_prev = previousFrame.pointable(pN.id());
+
+	if (!pN_prev.isValid())
+	continue; //Pointable has only been around for one frame
+
+	//Look for the transition from inside to outside of the touchzone
+	if (pN_prev.touchDistance() <= 0 && pN.touchDistance() > 0)
+	{
+		 printf(“Pointable %d tapped”,pN.id());
+		}
+	}
+
+	//We reached the frame processed last time
+	if (previousFrame.id() == lastFrame.id())
+		break;
+
+	}
+	lastFrame = controller.frame();
+}
+
+void onFrame(const Controller & controller)
+{
+	Frame currentFrame = controller.frame();
+	Frame lastFrame = controller.frame(1);
+
+	if (!lastFrame.isValid()) 
+	return; //Only one frame so far - do nothing
+
+
+	for (int p = 0; p < currentFrame.pointables().count();p++)
+	{
+		Pointable pN = currentFrame.pointables()[p];
+		Pointable pN_prev = lastFrame.pointable(pN.id());
+
+		if (!pN_prev.isValid())
+			continue; //Pointable has only been around for one frame
+
+		//Look for the transition from inside to outside of the touchzone
+		if (pN_prev.touchDistance() <= 0 && pN.touchDistance() > 0)
+		{
+			printf(“Pointable %d tapped”,pN.id());
+		}
+	}
+}
+
+
+int main()
+{
+	Controller controller;
+	TapDetector tapDetector;
+
+	while (true)
+	{
+		tapDetector.onFrame(controller);
+
+		//Other application logic
+
+		//Rendering 
+	}
+}
